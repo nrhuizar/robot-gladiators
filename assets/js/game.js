96110ed2236c551b89ce61ca3983ba8994b90084
nrhuizar <nrhuizar@gmail.com>
2020-10-24 09:59:12 -0700

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,9 +3,14 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = "Roborto";
+// You can also log multiple values at once like this
+console.log(playerName, playerHealth, playerAttack, playerMoney);
+
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+console.log(enemyAttack, enemyHealth, enemyNames);
 
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
@@ -16,22 +21,22 @@ var fight = function() {
         enemyHealth = enemyHealth - playerAttack;
         // Log a resulting message to the console so we know that it worked.
         console.log(
-            playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+            playerName + " attacked " + enemyNames+ ". " + enemyNames + " now has " + enemyHealth + " health remaining."
         );
         // check enemy's health
         if (enemyHealth <= 0) {
-            window.alert(enemyName + " had died!");
+            window.alert(enemyNames+ " had died!");
         }
         else {
-            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+            window.alert(enemyNames+ " still has " + enemyHealth + " health left.");
         }
         // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to updte the value in the 'playerHealth' variable.
             playerHealth = playerHealth - enemyAttack
         // Log a resulting message to the console so we know that it worked.
-        console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+        console.log(enemyNames+ " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
         // Check player's health
         if (playerHealth <= 0) {
-            window.alert(playerName + " has deid!");
+            window.alert(playerName + " has died!");
         }
         else {
             window.alert(playerName + " still has " + playerHealth + " health left.");
@@ -50,10 +55,10 @@ var fight = function() {
         else {
             fight();
         }
-        window.alert(playerName + " has chosen to skip the fight!");
-    } else {
+    }
+    else {
         window.alert("You need to choose a valid option. Try again!");
     }
 };
 
-fight();
+//fight();
